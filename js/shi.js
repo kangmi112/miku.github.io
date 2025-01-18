9b6068b2449186239c02eecba5827c215956a5b8
kangmi112 <3529642120@qq.com>
2025-01-18 23:33:22 +0800

    v0.34
    添加一些功能,添加当前页面

---
+++ js/shi.js
@@ -0,0 +1,26 @@
+let youshi = false;
+let shurukuang = document.getElementById("shurukuang");
+let anNiu = document.getElementById("anNiu");
+function change() {
+  if (youshi) {
+    // 此时按钮为✘，点击后变为✔，清空输入的内容
+    youshi = false;
+    anNiu.innerText = "✔";
+    shurukuang.style.borderBottomColor = "rgb(241, 154, 168)";
+    shurukuang.value = "";
+    shurukuang.readOnly = false;
+    window.localStorage.removeItem("shi");
+  } else {
+    // 此时按钮为✔，点击后变为✘,保存输入的内容
+    youshi = true;
+    anNiu.innerText = "✘";
+    shurukuang.style.borderBottomColor = "transparent";
+    shurukuang.readOnly = true;
+    window.localStorage.setItem("shi", shurukuang.value);
+  }
+}
+let data = window.localStorage.getItem("shi");
+if (data) {
+  document.getElementById("shurukuang").value = data;
+  change();
+}
